99a813054d4471f603b74f3b8f7c62ff7191cc7b
Danilo Noguera <danilo.noguera@gmail.com>
2022-10-14 18:01:34 -0600

    Se agregó el archivo OtroEjemploExcepciones.java

--- src/excepciones/OtroEjemploExcepciones.java
+++ src/excepciones/OtroEjemploExcepciones.java
@@ -1,16 +1,46 @@
 package excepciones;
 
+import java.util.NoSuchElementException;
+//import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class OtroEjemploExcepciones {
 
 	public static void main(String[] args) {
-		int x = LeerEntero();
+		int x = LeerEnteroSinExcepciones();
+		System.out.println("x = " + x);
+		int y = LeerEnteroConExcepciones();
+		System.out.println("y = " + y);
 	}
 	//Metodo que lea un entero
 	private static int LeerEntero() {
 		Scanner lector = new Scanner(System.in);
 		System.out.print("Ingrese un numero: ");
 		return lector.nextInt();
+	}
+	private static int LeerEnteroSinExcepciones() {
+		Scanner lector = new Scanner(System.in);
+		System.out.print("Ingrese un numero: ");
+		String entrada = lector.next();
+		boolean esEntero = true;
+		for (int i = 0; i < entrada.length(); i++) {
+			if (!Character.isDigit(entrada.charAt(i))) {
+				esEntero = false;
+				break;
+			}			
+		}
+		if (esEntero) {
+			return Integer.parseInt(entrada);
+		}
+		return 0;
+	}
+	private static int LeerEnteroConExcepciones() {
+		Scanner lector = new Scanner(System.in);
+		System.out.print("Ingrese un numero: ");
+		try {
+			return lector.nextInt();
+		} catch (NoSuchElementException e) {
+			return 0;
+		}
 	}
 }
